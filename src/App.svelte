96fd4c8d679980779526c7229a651b1787522378
Matej Capkovic <mcapkovic@gmail.com>
2021-01-31 11:44:18 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  //   export let name;
+</script>
+
+<main>
+  <div class="figure">
+    <img
+      src="/img/figure-all.png"
+      alt="figure"
+
+    />
+  </div>
+  
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+  .figure {
+	width: 240px;
+    height: 230px;
+    overflow: hidden;
+	border:1px solid red;
+  }
+</style>
