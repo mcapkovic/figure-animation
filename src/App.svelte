0cc2f4c9a1a5dc94c4b602b786cacc4a541ec3ba
Matej Capkovic <mcapkovic@gmail.com>
2021-02-10 07:51:25 +0100

    Add demo4

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,24 @@
   import Demo from "./components/example1/Demo.svelte";
   import Demo2 from "./components/example1/Demo2.svelte";
   import Demo3 from "./components/example1/Demo3.svelte";
+  import Demo4 from "./components/example2/Game.svelte";
 
-  let activeDemo = 2;
+  let activeDemo = 4;
 </script>
 
 <main>
-  <!-- <Demo3 />
-  <Demo2 /> -->
-
-  {#if activeDemo=== 2}
-  <Demo2 />
-
-  {:else}
-  <Demo3 />
+  {#if activeDemo === 2}
+    <Demo2 />
+  {:else if activeDemo === 3}
+    <Demo3 />
+  {:else if activeDemo === 4}
+    <Demo4 />
   {/if}
 
   <div class="controls">
     <button on:click={() => (activeDemo = 2)}>demo2</button>
     <button on:click={() => (activeDemo = 3)}>demo3</button>
+    <button on:click={() => (activeDemo = 4)}>demo4</button>
   </div>
 
   <Demo />
--- src/components/example2/Game.svelte
+++ src/components/example2/Game.svelte
@@ -1,7 +1,103 @@
 <script>
+  import { onMount } from "svelte";
+
+  let frames;
+
+  // SELECT CVS
+  let cvs;
+  let ctx;
+
+  // LOAD IMAGE
+  const layer_0000 = new Image();
+  const layer_0001 = new Image();
+  const layer_0002 = new Image();
+  const layer_0003 = new Image();
+  const layer_0004 = new Image();
+  const layer_0005 = new Image();
+  const layer_0006 = new Image();
+  const layer_0007 = new Image();
+  const layer_0008 = new Image();
+  const layer_0009 = new Image();
+  const layer_0010 = new Image();
+
+  layer_0000.src = "/img/background_layers/Layer_0000_9.png"
+  layer_0001.src = "/img/background_layers/Layer_0001_8.png";
+  layer_0002.src = "/img/background_layers/Layer_0002_7.png";
+  layer_0003.src = "/img/background_layers/Layer_0003_6.png";
+  layer_0004.src = "/img/background_layers/Layer_0004_Lights.png";
+  layer_0005.src = "/img/background_layers/Layer_0005_5.png";
+  layer_0006.src = "/img/background_layers/Layer_0006_4.png";
+  layer_0007.src = "/img/background_layers/Layer_0007_Lights.png";
+  layer_0008.src = "/img/background_layers/Layer_0008_3.png";
+  layer_0009.src = "/img/background_layers/Layer_0009_2.png";
+  layer_0010.src = "/img/background_layers/Layer_0010_1.png";
+
+
+  // BACKGROUND
+const bg = {
+    sX : 0,
+    sY : 0,
+    w : 928,
+    h : 793,
+    x : 0,
+    y :  0,
+    
+    draw : function(){
+        // console.log(ctx)
+        // ctx.drawImage(layer_0009, 0, 0);
+
+        // ctx.drawImage(layer_0009, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        
+        // ctx.drawImage(layer_0009, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+    
+}
+
+  // DRAW
+  function draw() {
+    ctx.fillStyle = "#7693b3";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    bg.draw();
+    // pipes.draw();
+    // fg.draw();
+    // bird.draw();
+    // getReady.draw();
+    // gameOver.draw();
+    // score.draw();
+  }
+
+  // UPDATE
+  function update() {
+    // bird.update();
+    // fg.update();
+    // pipes.update();
+  }
+
+  // LOOP
+  function loop() {
+    update();
+    draw();
+    frames++;
+    // console.log(frames)
+    requestAnimationFrame(loop);
+  }
+
+  onMount(() => {
+    cvs = document.querySelector("#game-canvas");
+    ctx = cvs.getContext("2d");
+
+    console.log(layer_0006);
+    loop();
+  });
 </script>
 
-<div>game</div>
+<canvas id="game-canvas" class="game" />
 
 <style>
+  .game {
+    height: 793px;
+    width: 90%;
+    border: 1px solid red;
+    /* margin: 1rem; */
+  }
 </style>
