51b17be664ee52619f2225fc483f0ece81973397
Matej Capkovic <mcapkovic@gmail.com>
2021-02-14 18:23:01 +0100

    Add play buttons style

--- src/components/example2/BackgroundPlayer.svelte
+++ src/components/example2/BackgroundPlayer.svelte
@@ -3,22 +3,35 @@
 
   const VISIBLE = "visible";
   const HIDDEN = "hidden";
+
   let player1;
   let videoVisibility = HIDDEN;
+  let videoState = -1;
+
   function playVideo() {
     // player1.seekTo(4);
     player1.playVideo();
   }
-  function stopVideo() {
-    // player1.seekTo(4, false);
-    player1.stopVideo();
+
+//   function stopVideo() {
+//     // player1.seekTo(4, false);
+//     player1.stopVideo();
+//   }
+
+  function pauseVideo() {
+    player1.pauseVideo();
   }
 </script>
 
 <div class="player-wrapper">
-  <div>
-    <button on:click={playVideo}>play</button>
-    <button on:click={() => player1.pauseVideo()}>pause</button>
+  <div class="player-wrapper__buttons">
+    {#if videoState === -1 || videoState === 2}
+      <button on:click={playVideo}>play</button>
+    {:else if videoState === 1}
+      <button on:click={pauseVideo}>pause</button>
+    {:else if videoState === 3}
+      <span>buffering</span>
+    {/if}
     <button
       on:click={() =>
         (videoVisibility = videoVisibility === VISIBLE ? HIDDEN : VISIBLE)}
@@ -28,25 +41,40 @@
   <div
     class={`player-wrapper__video player-wrapper__video--${videoVisibility}`}
   >
-    <YouTube videoId="EDRmJSwrVns" bind:this={player1} />
+    <YouTube
+      videoId="EDRmJSwrVns"
+      bind:this={player1}
+      on:StateChange={(e) => (videoState = e.detail)}
+    />
   </div>
 </div>
 
 <style>
   .player-wrapper {
     position: absolute;
-    top: 10px;
+    top: 1rem;
     /* right: 0; */
     left: 50%;
     transform: translateX(-50%);
+    text-align: center;
   }
-  .player-wrapper__video--visible{
-    /* opacity: 1; */
+  .player-wrapper__video--visible {
     display: block;
   }
-  .player-wrapper__video--hidden{
-    /* opacity: 0; */
+  .player-wrapper__video--hidden {
     display: none;
+  }
 
+  .player-wrapper__buttons {
+    color: white;
+  }
+  .player-wrapper__buttons button {
+    background: none;
+    color: white;
+    border: 1px solid white;
+  }
+
+  .player-wrapper__buttons button:active {
+    background-color: #ffffff17;
   }
 </style>
